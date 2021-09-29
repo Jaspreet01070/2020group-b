5c0dbe63c94d64189f51a02039653a3e6b398bfa
Chieh-Jung Lo <lo8385@saskpolytech.ca>
2021-09-29 19:02:48 +0000

    capstone_project_groupb/user12/db/models/contest-model.js edited online with Bitbucket

--- capstone_project_groupb/user12/db/models/contest-model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-var contestSchema = new mongoose.Schema({
-    cName: {type: String, required: true},
-    cPeriod: {type: Date, required: true},
-    cStatus: {type: Boolean, required: true}
-});
-
-module.exports = mongoose.model("Contest", contestSchema);
